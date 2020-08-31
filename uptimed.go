7cb386d5b00aea57815ed47fff196037b0bb53b2
4thel00z <4thel00z@gmail.com>
2020-08-31 13:12:16 +0200

    [master] Add uptimed.go

---
+++ uptimed.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func AppCleanup() {
+	log.Println("CLEANUP APP BEFORE EXIT!!!")
+}
+
+var (
+	url = flag.String(
+		"url",
+		os.Getenv("UPTIMED_URL"),
+		"Set the UPTIMED_URL env var or pass the url of the request that uptimed should do occasionally",
+	)
+)
+
+func main() {
+
+	flag.Parse()
+
+	UptimeCounter := time.Duration(30)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGQUIT)
+	signal.Notify(sigs, syscall.SIGINT)
+
+	go func() {
+		s := <-sigs
+		s.Signal()
+		log.Printf("RECEIVED SIGNAL: %s", s)
+		AppCleanup()
+		os.Exit(1)
+	}()
+
+	for {
+
+		req, err := http.NewRequest("GET", *url, nil)
+
+		if err != nil {
+			log.Println("Not reachable, sleeping...")
+			time.Sleep(UptimeCounter * time.Second)
+			panic("Bailing out")
+		}
+		log.Println("Reached mothership.")
+
+		_, err = http.DefaultClient.Do(req)
+
+		if err != nil {
+			log.Println("Not reachable, sleeping...")
+			time.Sleep(UptimeCounter * time.Second)
+			panic("Bailing out")
+		}
+
+		time.Sleep(UptimeCounter * time.Second)
+
+	}
+}
